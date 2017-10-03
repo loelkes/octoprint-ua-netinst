02c4d9fee76038e8b9993aaa6b14049924ca38c8
Christian Lölkes <christian.loelkes@gmail.com>
2017-10-03 17:30:05 +0200

    Added libjpeg and zlib1g packages

--- apa102.sh
+++ apa102.sh
@@ -8,13 +8,13 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 if [ $1 == 'firstboot' ]; then
 
 	# Overclocking
-	echo "arm_freq=1000" >> /boot/config.txt
-	echo "sdram_freq=500" >> /boot/config.txt
-	echo "core_freq=500" >> /boot/config.txt
-	echo "over_voltage=6" >> /boot/config.txt
-	echo "temp_limit=75" >> /boot/config.txt
-	echo "boot_delay=" >> /boot/config.txt
-	echo "dtparam=spi=on" >> /boot/config.txt #Enable SPI
+	echo "arm_freq=1000" 	>> /boot/config.txt
+	echo "sdram_freq=500" 	>> /boot/config.txt
+	echo "core_freq=500" 	>> /boot/config.txt
+	echo "over_voltage=6" 	>> /boot/config.txt
+	echo "temp_limit=75" 	>> /boot/config.txt
+	echo "boot_delay=" 	>> /boot/config.txt
+	echo "dtparam=spi=on" 	>> /boot/config.txt #Enable SPI
 
 	# Disable the splash screen
 	echo "disable_splash=1" >> /boot/config.txt
@@ -30,7 +30,7 @@ fi
 if [ $1 == 'secondboot' ]; then
 
 	# Install basic tools
-	apt-get -y install nano screen python3 git python3-pip raspi-config
+	apt-get -y install nano screen python3 git python3-pip raspi-config libjpeg8-dev zlib1g-dev
 	pip3 install spidev pillow numpy
   	cd ~
   	git clone https://github.com/tinue/APA102_Pi.git
